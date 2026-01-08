fa33abd97312b7cffc60347426665dd70dbe3763
Dorian Barber <dorianbarber@gmail.com>
2026-01-07 20:17:02 -0500

    Initial Commit

---
+++ habittrackerTests/habittrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  habittrackerTests.swift
+//  habittrackerTests
+//
+//  Created by Dorian Barber on 1/7/26.
+//
+
+import Testing
+@testable import habittracker
+
+struct habittrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
